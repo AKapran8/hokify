8f68dd693ffe0a0187a2835b938809a61bca2078
Andrii Kapran <kapranandriyu@gmail.com>
2023-11-04 22:29:26 +0100

    init project

---
+++ components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <header class="bg-header">
+    <nav class="container mx-auto p-3 flex justify-between">
+      <NuxtLink to="/" >
+        <img src="./../static/logo.svg" class="h-9 inline-block" alt="Logo">
+      </NuxtLink>
+      <ul class="flex gap-4">
+        <li class="cursor-pointer text-white hover:border-b-4 font-bold transition-transform duration-300">
+          <NuxtLink to="/">Form</NuxtLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style>
+.bg-header {
+  background: #0fb1af;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Header />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+interface Main {
+  age: number;
+  name: string;
+}
+export default Vue.extend({
+  name: 'IndexPage',
+  age: 25,
+} as Main)
+</script> 
