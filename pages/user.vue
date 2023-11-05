7607c4360059069a2a53d5dcebb66b2a9fe53c59
Andrii Kapran <kapranandriyu@gmail.com>
2023-11-05 19:40:44 +0100

    updated user info page

--- pages/user.vue
+++ pages/user.vue
@@ -1,14 +1,27 @@
 <template>
   <div class="mt-10 mx-auto mb-0">
-    <h1 class="text-2xl font-semibold mb-4">Information</h1>
-    <p class="text-gray-700">Nachname: {{ user.name }}</p>
-    <p class="text-gray-700">Vorname: {{ user.lastName }}</p>
-    <p class="text-gray-700">Email: {{ user.email }}</p>
-    <p class="text-gray-700">Geschlecht: {{ user.gender }}</p>
-    <p class="text-gray-700">Nachricht: {{ user.note }}</p>
+    <h1 class="text-2xl font-semibold mb-4">Nutzerinformation</h1>
+    <div v-if="user.name.length">
+      <p class="text-gray-700">Nachname: {{ user.name }}</p>
+      <p class="text-gray-700">Vorname: {{ user.lastName }}</p>
+      <p class="text-gray-700">Email: {{ user.email }}</p>
+      <p class="text-gray-700">Geschlecht: {{ user.gender }}</p>
+      <p class="text-gray-700">Nachricht: {{ user.note }}</p>
+    </div>
+    <p v-else class="text-gray-700">
+      Keine Information. Bitte legen Sie neue Benutzerdaten fest.
+    </p>
+    <button
+      v-if="user.name.length"
+      type="button"
+      class="inline-block rounded-full border px-11 py-1 btn-color mt-3"
+      @click="resetUserData"
+    >
+      Daten zurücksetzen
+    </button>
   </div>
 </template>
-<script>
+<script lang="ts">
 import Vue from 'vue'
 
 export default Vue.extend({
@@ -21,5 +34,24 @@ export default Vue.extend({
       return this.$store.getters.getUser
     },
   },
+  methods: {
+    resetUserData(): void {
+      this.$store.dispatch('resetUser')
+    },
+  },
 })
 </script>
+
+<style scoped>
+.btn-color {
+  border-color: #0fb1af;
+  background-color: #0fb1af;
+  color: #fff;
+}
+
+.btn-color:hover {
+  border-color: #106463;
+  background-color: #106463;
+  transition: 0.5s ease;
+}
+</style>
--- store/index.ts
+++ store/index.ts
@@ -1,4 +1,4 @@
-import { IUserData } from '~/models/user-data.model';
+import { IUserData } from '~/models/user-data.model'
 
 export const state = () => ({
   user: {
@@ -14,40 +14,43 @@ export const state = () => ({
     email: '',
     gender: '',
     note: '',
-  } as IUserData,
+  } as IUserData, // Not sure, I think it useless
   isSubmitted: false,
-});
+})
 
 export const mutations = {
-  setUser(state: { user: IUserData; isSubmitted: boolean} , data: IUserData) {
-    state.user = data;
-    state.isSubmitted = true;
+  setUser(state: { user: IUserData; isSubmitted: boolean }, data: IUserData) {
+    state.user = data
+    state.isSubmitted = true
   },
-  resetUser(state: { user: IUserData; }) {
+  resetUser(state: { user: IUserData }) {
     state.user = {
       name: '',
       lastName: '',
       email: '',
       gender: '',
       note: '',
-    };
+    }
   },
 }
 
 export const getters = {
-  getUser(state: { user: IUserData; }) {
+  getUser(state: { user: IUserData }) {
     return state.user
   },
   getEmptyUser(state: { user: IUserData; emptyUser: IUserData }) {
     return state.emptyUser
   },
-  getSubmitStatus(state: { user: IUserData; isSubmitted: boolean}) {
+  getSubmitStatus(state: { user: IUserData; isSubmitted: boolean }) {
     return state.isSubmitted
-  }
+  },
 }
 
 export const actions = {
-  createUser({commit}: any, userData: IUserData) {
+  createUser({ commit }: any, userData: IUserData) {
     commit('setUser', userData)
-  }
+  },
+  resetUser({ commit }: any) {
+    commit('resetUser')
+  },
 }
