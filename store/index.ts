7d026f0b1744848ad5e16ad71720ccadd6b41f29
Andrii Kapran <kapranandriyu@gmail.com>
2023-11-05 16:22:50 +0100

    add empty user func to store

--- store/index.ts
+++ store/index.ts
@@ -8,25 +8,41 @@ export const state = () => ({
     gender: '',
     note: '',
   } as IUserData,
+  emptyUser: {
+    name: '',
+    lastName: '',
+    email: '',
+    gender: '',
+    note: '',
+  } as IUserData,
 });
 
 export const mutations = {
   setUser(state: { user: IUserData; } , data: IUserData) {
-    console.log('data ', data)
     state.user = data;
-  }
+  },
+  resetUser(state: { user: IUserData; }) {
+    state.user = {
+      name: '',
+      lastName: '',
+      email: '',
+      gender: '',
+      note: '',
+    };
+  },
 }
 
 export const getters = {
   getUser(state: { user: IUserData; }) {
     return state.user
+  },
+  getEmptyUser(state: { user: IUserData; emptyUser: IUserData }) {
+    return state.emptyUser
   }
-
 }
 
 export const actions = {
   createUser({commit}: any, userData: IUserData) {
-    console.log('userData ', userData)
     commit('setUser', userData)
   }
 }
